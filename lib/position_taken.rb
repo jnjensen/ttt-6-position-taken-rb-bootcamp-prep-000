875ffe1fd486786c3f35b4a90dc11078bb6b36d7
James Jensen <jnjensen@live.com>
2020-03-26 02:09:24 +0000

    Automatically backed up by Learn

--- lib/position_taken.rb
+++ lib/position_taken.rb
@@ -4,7 +4,7 @@ def position_taken?(array, token_index)
   if array[token_index] == " " || array[token_index] == "" || array[token_index] == nil
     return false
   
-  elsif array[token_index] == "X" || array[token_index] "O"
+  elsif array[token_index] == "X" || array[token_index] == "O"
     return true
     
   end
