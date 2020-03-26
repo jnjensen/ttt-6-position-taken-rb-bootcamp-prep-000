0f353e1816f8381f2f0a6f2d1107aadbb0f0b763
James Jensen <jnjensen@live.com>
2020-03-26 02:03:54 +0000

    Automatically backed up by Learn

--- lib/position_taken.rb
+++ lib/position_taken.rb
@@ -1,7 +1,7 @@
 # code your #position_taken? method here!
 
 def position_taken?(array, token_index)
-  if array[token_index] == " "
+  if array[token_index] == " " || array[token_index] == ""
     return FALSE
   end
 end
